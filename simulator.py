970739ba5b3bd8d1832f052580bd348081b53a0c
Brian Wedel <brian.wedel@gmail.com>
2019-02-03 13:32:31 -0700

    WIP SAVE for simulator.py

--- simulator.py
+++ simulator.py
@@ -13,7 +13,8 @@ class TimeTickEvent(Event):
         self.type = Event.TIME
 
 class PowerEvent(Event):
-    def __init__(self, source, power_value_watts):
+    def __init__(self, time, source, power_value_watts):
+        self.time = time
         self.source = source
         self.value = power_value_watts
         self.type = Event.POWER
@@ -34,36 +35,35 @@ class Sim:
         for e in Event.events()
             self.event_callbacks[e] = []
 
-    def add_event_callback(self, event_type, dest_node, callback):
-       self.event_callbacks[event_Type].append(EventCallback(event_type, dest_node, callback))
+    def add_event_callback(self, event_type, callback):
+       self.event_callbacks[event_Type].append(callback)
 
     def add_node(self, node):
         self.nodes.append(node)
         node.register(self)
 
     def generate_event(self, event):
-        self.pending_events.append(event)
-
-    def _run_events(self):
-        for cb in self.event_callbacks[event.type]:
-            cb.callback(event)
+        self.event_list.insert(event)
 
     def run(self, duration_sec):
         for t in range(0, duration_sec):
-            # Simulator generated time tick
-            time_tick = TimeTickEvent(self, t)
+            time_tick = TimeTickEvent(t, self, t)
             self.generate_event(time_tick)
 
-            # Service all other generated events (from other nodes)
-            
+        # Run events until event list is empty
+        while len(self.event_list) > 0:
+            e = self.event_list.pop()
+            for f in self.event_callbacks[e.type]:
+                f(e)
 
-class HouseLoad:
+class HouseLoad(Node):
     def register(self, sim):
         self.sim = sim
-        self.sim.add_event_callback(Event.TIME, self, self.time_tick_callback)
+        self.sim.add_event_callback(Event.TIME, self.time_tick_callback)
 
     def time_tick_callback(self, event):
-        self.sim.generate_event(EventPower(self, 1000.0))
+        self.update_time(event)
+        self.sim.generate_event(EventPower(self.time(), self, 1000.0))
 
 if __name__ == "__main__":
     house = HouseLoad()
