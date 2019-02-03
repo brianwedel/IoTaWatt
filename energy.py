d05213c215c43f856839dff0818f5ce4c64ea154
Brian Wedel <brian.wedel@gmail.com>
2019-02-03 10:21:30 -0700

    Re-calculate energy usage when plot view changes

--- energy.py
+++ energy.py
@@ -11,6 +11,7 @@ import json
 import time
 import datetime
 import pathlib
+import pytz
 
 class DocumentCache:
     """ Cache documents from firestore database locally in a file.
@@ -112,33 +113,66 @@ class EnergyCalculator:
 
         print("Number of samples: {}".format(len(self.power_meas)))
 
-    def energy_kwh(self):
+    def energy_kwh(self, start_time, end_time):
         if len(self.timestamp) == 0:
             return 0
         else:
-            r = integrate.simps(self.power_meas, self.timestamp)
+            # find start index
+            for t_idx, t_val in enumerate(self.timestamp):
+                if t_val >= start_time:
+                    start_time_index = t_idx
+                    break
+
+            # find end index (choose last index of search for boundary)
+            if end_time >= self.timestamp[-1]:
+                end_time_index = len(self.timestamp) - 1
+            else:
+                for t_idx, t_val in enumerate(self.timestamp[start_time_index:]):
+                    if t_val > end_time:
+                        end_time_index = t_idx + start_time_index # might be a off by one issue here =)
+                        break
+            
+            r = integrate.simps(
+                self.power_meas[start_time_index:end_time_index],
+                self.timestamp[start_time_index:end_time_index])
 
             # Convert joules (ws) to kwh
-            return r/(1000*60*60)
+            energy = r/(1000*60*60)
+            print("Energy: {} kwh".format(round(energy, ndigits=3)))
+            return energy
 
     def plot(self):
-        dt = [datetime.datetime.fromtimestamp(t) for t in self.timestamp]
+        # timestamps from DB are utc
+        dt = [datetime.datetime.utcfromtimestamp(t) for t in self.timestamp]
         plt.subplots_adjust(bottom=0.2)
         plt.xticks( rotation=25 )
         ax=plt.gca()
         xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
         ax.xaxis.set_major_formatter(xfmt)
 
-        ax.callbacks.connect('xlim_changed', lambda axes : print(ax.get_xlim()))
+        ax.callbacks.connect('xlim_changed', lambda axes : self.xlim_change_callback(axes))
         plt.plot(md.date2num(dt), self.power_meas)
         plt.show()
 
+    def xlim_change_callback(self, axes):
+        x_range = axes.get_xlim()
+        new_start_utc_dt = md.num2date(x_range[0])
+        new_end_utc_dt = md.num2date(x_range[1])
+        print("\nNew X-Axis range ({},{})".format(new_start_utc_dt, new_end_utc_dt))
+        new_start = new_start_utc_dt.timestamp()
+        new_end   = new_end_utc_dt.timestamp()
+        self.energy_kwh(new_start, new_end)
+
+
 if __name__ == "__main__":
 
+
+    # mktime takes local time_struct tuple and converts to seconds since the epoch
     start_time = time.mktime((2019, 2, 2, 17, 0, 0, -1, -1, -1))
-    end_time = time.mktime((2019, 2, 3, 0, 0, 0, -1, -1, -1))
+    end_time = time.mktime((2019, 2, 3, 17, 0, 0, -1, -1, -1))
+    print("initial start {}".format(start_time))
+    print("initial end   {}".format(end_time))
 
     e = EnergyCalculator(start_time, end_time)
-
-    print("Energy: {} kwh".format(round(e.energy_kwh(), ndigits=3)))
+    e.energy_kwh(start_time, end_time)
     e.plot()
