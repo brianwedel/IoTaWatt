5e37ec0fbdaacd59b48d1c06e2df159a70adb2eb
EC2 Default User <ec2-user@ip-172-31-30-162.us-west-2.compute.internal>
2019-02-15 20:22:45 +0000

    Add AWS proxy server

---
+++ aws_iot_proxy.py
@@ -0,0 +1,137 @@
+'''
+/*
+ * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+ '''
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
+import time
+import argparse
+import json
+import socket
+
+# Proxy server for Firestore database
+# Listen for UDP datagrams on port 12000, expecting a JSON string payload
+# Read JSON into a dictionary and set into the Firestore database
+# Proxy server is meant to be a pass through for data payload and creates
+# a new Firestore db document for each received payload.  The proxy server
+# uses the local time (ms) as the document name
+
+HOUSE_ID = "7905 Wellshire Ct"
+def reset_accum_samples():
+    current_time_ms = int(round(time.time() * 1000))
+    accum_samples = {
+        "house_id" : HOUSE_ID,
+        "timestamp" : current_time_ms,
+        "samples" : []}
+    return accum_samples
+
+# Create server socket
+soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Assign IP address and port number to socket
+soc.bind(('', 12000))
+
+# Server loop to forward data to firestore database
+first_sample = True
+
+# Read in command-line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
+parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
+parser.add_argument("-p", "--port", action="store", dest="port", type=int, help="Port number override")
+parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
+                    help="Use MQTT over WebSocket")
+parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
+                    help="Targeted client id")
+
+args = parser.parse_args()
+host = args.host
+rootCAPath = args.rootCAPath
+certificatePath = args.certificatePath
+privateKeyPath = args.privateKeyPath
+port = args.port
+useWebsocket = args.useWebsocket
+clientId = args.clientId
+topic = "meas"
+
+if args.useWebsocket and args.certificatePath and args.privateKeyPath:
+    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
+    exit(2)
+
+if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
+    parser.error("Missing credentials for authentication.")
+    exit(2)
+
+# Port defaults
+if args.useWebsocket and not args.port:  # When no port override for WebSocket, default to 443
+    port = 443
+if not args.useWebsocket and not args.port:  # When no port override for non-WebSocket, default to 8883
+    port = 8883
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+if useWebsocket:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
+    myAWSIoTMQTTClient.configureEndpoint(host, port)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+else:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+    myAWSIoTMQTTClient.configureEndpoint(host, port)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+time.sleep(2)
+
+# Server loop to forward data to firestore database
+while True:
+    # Reset sample struct each 40 samples    
+    accum_samples = reset_accum_samples()
+
+    for s in range(0, 40):
+        # Receive the client packet along with the address it is coming from
+        message, address = soc.recvfrom(1024)
+        try:
+            data = json.loads(message)
+        except json.JSONDecodeError:
+            logging.error("Invalid data payload {}".format(message))
+            data = {}
+
+        accum_samples["samples"].append(data)
+
+    # At the end of sample accumulation, foward onto AWS IOT core
+    try:
+        messageJson = json.dumps(accum_samples)
+        myAWSIoTMQTTClient.publish(topic, messageJson, 1)
+    except Exception as e:
+        logging.error("Exception occured while publishing to AWS IOT: " + str(e))
