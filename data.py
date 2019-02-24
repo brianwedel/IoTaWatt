8a9fc9bbf99a4e01fca3fd5ce6dcbb8122345ef9
Brian Wedel <brian.wedel@gmail.com>
2019-02-24 15:40:34 -0700

    Save work

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ from boto3.dynamodb.conditions import Key, Attr
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('wellshire-testbed-agg2')
 
-today="1550646000"
+today="1550905200"
 data = {}
 
 for node_id in range(1,31):
@@ -18,14 +18,20 @@ for node_id in range(1,31):
 
 
 def compare_dict(d1, d2):
-   diffkeys = [k for k in d1 if d1[k] != d2[k] and k != "house_id"]
+   diffkeys = [k for k in d1 if d1[k] != d2[k] and k not in ["house_id", "dup_detect_request_id"]]
    for k in diffkeys:
       print("{} : {} != {}".format(k, d1[k], d2[k]))
+   return len(diffkeys)
 
-ref_key = "replicator1"
-reference = data[ref_key]
-for key, val in data.items():
-   print("Compare({}, {})".format(ref_key, key))
-   for m1, m2 in zip(reference, val): 
-      compare_dict(m1, m2) 
+for ref_key, ref_val in data.items():
+   for key, val in data.items():
+      print("Compare({}, {})".format(ref_key, key))
+      print("Length({}, {})".format(len(ref_val), len(val)))
+      count = 0
+      # skip last element because it's the active time bucket
+      for m1, m2 in zip(ref_val[:-1], val[:-1]): 
+         mismatch = compare_dict(m1, m2)
+         if mismatch > 0:
+             print("MISMATCH index {} {}\n{}".format(count, m1, m2))
+         count += 1 
    
